b086076f62643d6394a05f015d50516b52c08430
Carolina Mendoza <carolina.mendoza@epn.edu.ec>
2022-03-01 15:13:57 -0500

    añadir layout main

--- app/src/main/java/com/mendozacarolina/pruebasproyecto/LoginActivity.kt
+++ app/src/main/java/com/mendozacarolina/pruebasproyecto/LoginActivity.kt
@@ -20,7 +20,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         editTextEmail = findViewById(R.id.editTextEmail)
         editTextPassword = findViewById(R.id.editTextPassword)
