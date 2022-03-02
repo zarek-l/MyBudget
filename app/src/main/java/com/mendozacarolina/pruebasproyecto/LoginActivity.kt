f419f652ceed0235e0f25b222e83ab9cfc9bde95
Elvis Montaluisa <elvis.montaluisa@epn.edu.ec>
2022-03-02 10:19:34 -0500

    Implementación Log In

--- app/src/main/java/com/mendozacarolina/pruebasproyecto/LoginActivity.kt
+++ app/src/main/java/com/mendozacarolina/pruebasproyecto/LoginActivity.kt
@@ -1,35 +1,95 @@
 package com.mendozacarolina.pruebasproyecto
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var editTextEmail: EditText
+    //variables para LogIn
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var editTextEmail:EditText
     lateinit var editTextPassword: EditText
-    lateinit var buttonIngresar: Button
-    lateinit var buttonRegistrar: Button
+    lateinit var textViewRegistrar : TextView
+
+    //variables para los botones
+    lateinit var buttonLogin: Button
+
+
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
 
-        editTextEmail = findViewById(R.id.editTextEmail)
+        //inicializacion de variables
+        editTextEmail = findViewById(R.id.editTextEmailAddress)
         editTextPassword = findViewById(R.id.editTextPassword)
-        buttonIngresar = findViewById(R.id.buttonIngresar)
-        buttonRegistrar = findViewById(R.id.buttonRegistrar)
+        buttonLogin = findViewById(R.id.buttonLogin)
 
-        // Initialize Firebase Auth
-        auth = Firebase.auth
+        textViewRegistrar = findViewById(R.id.textViewRegistrar)
+        auth = FirebaseAuth.getInstance();
 
-        //Eventos clic
-
+        //LeerDatosDeArchivoPreferencias()
+        buttonLogin.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            if (!ValidarDatosRequeridos())
+                return@setOnClickListener
+            // EscribirDatosEnArchivoPreferencias()
+            AutenticarUsuario(email, password)
         }
 
+        textViewRegistrar.setOnClickListener{
+            //Toast.makeText(this@MainActivity, "You clicked on TextView 'Click Me'.", Toast.LENGTH_SHORT).show()
+           // var intent = Intent(this,CrearActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun AutenticarUsuario(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(EXTRA_LOGIN, "Exito")
+                    var intent = Intent(this,MainActivity::class.java)
+                    intent.putExtra(LOGIN_KEY,auth.currentUser!!.email)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Log.d(EXTRA_LOGIN, "Fallo")
+                    Toast.makeText(baseContext, task.exception!!.message,
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun ValidarDatosRequeridos():Boolean{
+        val email = editTextEmail.text.toString()
+        val clave = editTextPassword.text.toString()
+        if (email.isEmpty()) {
+            editTextEmail.setError("El email es obligatorio")
+            editTextEmail.requestFocus()
+            return false
+        }
+        if (clave.isEmpty()) {
+            editTextPassword.setError("La clave es obligatoria")
+            editTextPassword.requestFocus()
+            return false
+        }
+        if (clave.length < 3) {
+            editTextPassword.setError("La clave debe tener al menos 3 caracteres")
+            editTextPassword.requestFocus()
+            return false
+        }
+        return true
+    }
 }
